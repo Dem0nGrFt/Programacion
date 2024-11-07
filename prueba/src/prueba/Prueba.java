8f2018313b7d62a721ba4771566e57d4abd47b19
Dem0nGrFt <157622302+Dem0nGrFt@users.noreply.github.com>
2024-11-07 09:30:27 +0100

    Añadidos PC Clase

---
+++ prueba/src/prueba/Prueba.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package prueba;
+
+/**
+ *
+ * @author aitor.martinezparente
+ */
+public class Prueba {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int numbers[] = new int[5];
+        
+        numbers[3] = 6;
+        
+        for (int i=0; i<= numbers.length; i++) {
+            System.out.println(numbers[i]);
+        }
+    }
+    
+}
