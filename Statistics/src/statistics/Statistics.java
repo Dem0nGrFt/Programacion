333c4432de74eeacc7e6198da8f2545b0dc8b530
Dem0nGrFt <157622302+Dem0nGrFt@users.noreply.github.com>
2024-11-15 14:32:21 +0100

    avances en clase 15-11-24

--- Statistics/src/statistics/Statistics.java
+++ Statistics/src/statistics/Statistics.java
@@ -35,21 +35,28 @@ public class Statistics {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        Statistics stat = new Statistics();
 
+        stat.fillArrayLength();
+        stat.fillNumbers();
+        stat.median();
     }
 
     public void fillArrayLength() {
         Scanner scan = new Scanner(System.in);
 
         do {
-            System.out.println("¿Cuantos numeros vas a introducir? ");
+            System.out.println("¿Cuantos numeros vas a introducir? (debe ser impar): ");
             arrayLength = scan.nextInt();
-        } while (arrayLength / 2 != 0);
+            if (arrayLength % 2 == 0) {
+                System.out.println("Debe de ser un numero impar!!");
+            }
+        } while (arrayLength % 2 == 0);
         numbers = new int[arrayLength];
 
     }
 
-    public void fillMarks() {
+    public void fillNumbers() {
         Scanner scan = new Scanner(System.in);
 
         for (int i = 0; i < numbers.length; i++) {
@@ -57,6 +64,35 @@ public class Statistics {
             numbers[i] = scan.nextInt();
 
         }
+    }
+
+    public int median() {
+
+        for (int i = 0; i < numbers.length; i++) {
+
+            int biggers = 0;
+            int smallers = 0;
+
+            for (int j = 0; j < numbers.length; j++) {
+                if (j != i) {
+                    if (numbers[j] >= numbers[i]) {
+                        biggers++;
+                    }
+                    if (numbers[j] <= numbers[i]) {
+                        smallers++;
+                    }
+                }
+            }
+
+            System.out.println("hay " + biggers + " elementos mayores que el que esta en " + i);
+            System.out.println("hay " + smallers + " elementos menores que el que esta en " + i);
+
+            if (biggers >= numbers.length/2 && smallers >= numbers.length/2) {
+                System.out.println(numbers[i] + " es la mediana.");
+            }
+        }
+
+        return 0;
     }
 
 }
