a08869de1ed733e9f8fa920ae4d7e14e0135b303
Dem0nGrFt <157622302+Dem0nGrFt@users.noreply.github.com>
2024-11-26 11:12:42 +0100

    Avances en clase

---
+++ SortedArray/src/sortedarray/SortedArray.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package sortedarray;
+
+/**
+ *
+ * @author aitor.martinezparente
+ */
+public class SortedArray {
+
+    private int array[] = new int[6];
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+    public void sort(){
+        
+    }
+    
+    public void 
+
+}
